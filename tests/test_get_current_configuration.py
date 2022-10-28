6edecbd1bdfa60d69e754653612e6a95da676bad
Adam Lynam <adam@lynam.co.nz>
2022-10-28 16:19:33 +1300

    Makes sense to test for HTTP status 200 as well

--- tests/test_get_current_configuration.py
+++ tests/test_get_current_configuration.py
@@ -1,3 +1,5 @@
 def test_get_current_sensor_value(client):
     response = client.get("/sensors/1")
+
+    assert response.status_code == 200
     assert response.json["data"]["current"]["temperature"] == 18
