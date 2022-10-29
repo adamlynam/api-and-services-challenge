05bb647b67b0bee677d3e60d84e3700a5bbf4c82
Adam Lynam <adam@lynam.co.nz>
2022-10-30 11:22:54 +1300

    Shift response formatting responsibility to HTTP layer
    This isn't quite right, but we need the tests to pass before we make major changes to the functionality.

--- datahub/sensors.py
+++ datahub/sensors.py
@@ -8,9 +8,11 @@ bp = Blueprint('sensors', __name__, url_prefix='/sensors')
 @bp.route('/<sensor_id>', methods=['GET'])
 def sensors(sensor_id):
     persistence_service = PersistenceService()
-    return jsonify(
-        persistence_service.get(sensor_id)
-    )
+    return jsonify({
+        "data": {
+            "current": persistence_service.get(sensor_id)
+        }
+    })
 
 
 @ bp.route('/<sensor_id>', methods=['PATCH'])
