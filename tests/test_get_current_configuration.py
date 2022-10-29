e1b6572211c857e64b4e6e55920f7e7a79a72ced
Adam Lynam <adam@lynam.co.nz>
2022-10-29 16:42:07 +1300

    Refactor tests directory structure

--- tests/test_get_current_configuration.py
+++
@@ -1,5 +0,0 @@
-def test_get_current_sensor_value(client):
-    response = client.get("/sensors/1")
-
-    assert response.status_code == 200
-    assert response.json["data"]["current"]["temperature"] == 18
--- tests/unit/test_api_contracts.py
+++ tests/unit/test_api_contracts.py
@@ -1,3 +1,10 @@
+def test_get_current_sensor_value(client):
+    response = client.get("/sensors/1")
+
+    assert response.status_code == 200
+    assert response.json["data"]["current"]["temperature"] == 18
+
+
 def test_post_latest_sensor_value(client):
     response = client.post("/sensors/", json={
         "data": {
