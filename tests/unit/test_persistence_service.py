a386390420f26016b3817243ad7ca8a3b45afab1
Adam Lynam <adam@lynam.co.nz>
2022-10-30 15:50:11 +1300

    Add test describing the behaviour when key doesn't exist

--- tests/unit/test_persistence_service.py
+++ tests/unit/test_persistence_service.py
@@ -9,3 +9,9 @@ def test_after_saving_configuraton_it_can_be_retrieved():
     })
 
     assert persistence_service.get(1)["test_sensor_values"] == 123
+
+
+def test_retreiving_configuration_that_doesnt_exist_returns_none():
+    persistence_service = InMemoryPersistenceService()
+
+    assert persistence_service.get("doesnt exist") == None
